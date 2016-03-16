e2dd17a477753fab757449a40283bdb0fa06342f
Marcel Teixeira <marceltex@gmail.com>
2016-03-16 11:05:17 +0200

    Added more data about each club to JSON array

--- clubs.js
+++ clubs.js
@@ -1,24 +1,24 @@
 var clubData2016 = [
-{"name":"Leicester City", "netWorth":"133", "goalDif":22},
-{"name":"Tottenham Hotspur", "netWorth":"710", "goalDif":29},
-{"name":"Arsenal", "netWorth":"1118", "goalDif":16},
-{"name":"Manchester City", "netWorth":"907", "goalDif":21},
-{"name":"West Ham United", "netWorth":"215", "goalDif":12},
-{"name":"Manchester United", "netWorth":"1848", "goalDif":10},
-{"name":"Southampton", "netWorth":"259", "goalDif":8},
-{"name":"Liverpool", "netWorth":"537", "goalDif":6},
-{"name":"Stoke City", "netWorth":"192", "goalDif":-4},
-{"name":"Chelsea", "netWorth":"826", "goalDif":4},
-{"name":"West Bromwich Albion", "netWorth":"175", "goalDif":-6},
-{"name":"Everton", "netWorth":"216", "goalDif":12},
-{"name":"Bournemouth", "netWorth":"104", "goalDif":-9},
-{"name":"Watford", "netWorth":"133", "goalDif":-1},
-{"name":"Crystal Palace", "netWorth":"186", "goalDif":-7},
-{"name":"Swansea City", "netWorth":"183", "goalDif":-10},
-{"name":"Sunderland", "netWorth":"121", "goalDif":-19},
-{"name":"Norwich City", "netWorth":"166", "goalDif":-23},
-{"name":"Newcastle United", "netWorth":"221", "goalDif":-26},
-{"name":"Aston Villa", "netWorth":"165", "goalDif":-35}
+{"name":"Leicester City", "netWorth":133, "w":18, "d":9, "l":3, "gf":53, "ga":31, "gd":22, "pts":63},
+{"name":"Tottenham Hotspur", "netWorth":710, "w":16, "d":10, "l":4, "gf":53, "ga":24, "gd":29, "pts":58},
+{"name":"Arsenal", "netWorth":1118, "w":15, "d":7, "l":7, "gf":46, "ga"30, "gd":16, "pts":52},
+{"name":"Manchester City", "netWorth":907, "w":15, "d":6, "l":8, "gf":52, "ga":31, "gd":21, "pts":51},
+{"name":"West Ham United", "netWorth":215, "w":13, "d":10, "l":6, "gf":45, "ga":33, "gd":12, "pts":49},
+{"name":"Manchester United", "netWorth":1848, "w":13, "d":8, "l":8, "gf":37, "ga":27, "gd":10, "pts":47},
+{"name":"Southampton", "netWorth":259, "w":12, "d":8, "l":10, "gf":38, "ga":30, "gd":8, "pts":44},
+{"name":"Liverpool", "netWorth":537, "w":12, "d":8, "l":8, "gf":43, "ga":37, "gd":6, "pts":44},
+{"name":"Stoke City", "netWorth":192, "w":12, "d":7, "l":11, "gf":32, "ga":36, "gd":-4, "pts":43},
+{"name":"Chelsea", "netWorth":826, "w":10, "d":10, "l":9, "gf":43, "ga":39, "gd":4, "pts":40},
+{"name":"West Bromwich Albion", "netWorth":175, "w":10, "d":9, "l":10, "gf":30, "ga":36, "gd":-6, "pts":39},
+{"name":"Everton", "netWorth":216, "w":9, "d":11, "l":8, "gf":51, "ga":39, "gd":12, "pts":38},
+{"name":"Bournemouth", "netWorth":104, "w":10, "d":8, "l":12, "gf":38, "ga":47, "gd":-9, "pts":38},
+{"name":"Watford", "netWorth":133, "w":10, "d":7, "l":12, "gf":29, "ga":30, "gd":-1, "pts":37},
+{"name":"Crystal Palace", "netWorth":186, "w":9, "d":6, "l":14, "gf":32, "ga":39, "gd":-7, "pts":33},
+{"name":"Swansea City", "netWorth":183, "w":8, "d":9, "l":13, "gf":30, "ga":40, "gd":-10, "pts":33},
+{"name":"Sunderland", "netWorth":121, "w":6, "d":7, "l":16, "gf":35, "ga":54, "gd":-19, "pts":25},
+{"name":"Norwich City", "netWorth":166, "w":6, "d":7, "l":17, "gf":31, "ga":54, "gd":-23, "pts":25},
+{"name":"Newcastle United", "netWorth":221, "w":6, "d":6, "l":17, "gf":28, "ga":54, "gd":-26, "pts":24},
+{"name":"Aston Villa", "netWorth":165, "w":3, "d":7, "l":20, "gf":22, "ga":57, "gd":-35, "pts":16}
 ]
 
 function addClubs() {
@@ -34,9 +34,9 @@ function addClubs() {
 	for (var i = 0; i < clubData2016.length; i++) {
 		var height = (1 - (clubData2016[i].netWorth/2000))*100;
 		var haloColor;
-		if (clubData2016[i].goalDif > 0) {
+		if (clubData2016[i].gd > 0) {
 			haloColor = "halo-blue";
-		} else if (clubData2016[i].goalDif < 0) {
+		} else if (clubData2016[i].gd < 0) {
 			haloColor = "halo-red";
 		} else {
 			// Handle zero goal difference here
